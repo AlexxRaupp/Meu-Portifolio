7b4be4359bec76d623c5337004c1bbcafe641a18
AlexxRaupp <alexsanderferreiraraupp@gmail.com>
2023-08-16 21:59:31 -0300

    Meu Portifolio

---
+++ java/script.js
@@ -0,0 +1,49 @@
+new TypeIt("#companionMethods", {
+    speed: 300,
+    waitUntilVisible: true,
+    loop: true
+  })
+    .type("lex", { delay: 300 })
+    .move(-3)
+    .delete(1)
+    .type("A")
+    .move(null, { to: "END" })
+    .type(" Rauppx")
+    .pause(300)
+    .delete(2)
+    .type("p")
+    .move(-4)
+    .type("")
+    .move(null, { to: "END" })
+    .type("")
+    .pause(500)
+    .type("")
+    .go();
+
+
+    new TypeIt("#textoanimate", {
+        speed: 35,
+    waitUntilVisible: true,
+  })
+    .type("Uma apaixonado por Front<3And/Back-end", { delay: 300 })
+    .move(-12)
+    .delete(2)
+    .type("-")
+    .move(null, { to: "END" })
+    .break({ delay: 500 })
+    .type("Atualmente estudandp")
+    .pause(300)
+    .delete(1)
+    .type("o")
+    .break({ delay: 500 })
+    .pause(800)
+    .type("Css, Html, JavaScript, Python")
+    .move(null, { to: "END" })
+    .type("")
+    .pause(1000)
+    .type("")
+    .go();
+
+
+
+    
